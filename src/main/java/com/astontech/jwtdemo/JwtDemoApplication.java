1c7633a747e80a338fd0d4f3b604ff8bf13e472d
Connor.Swanson <swanscon@gmail.com>
2024-02-21 09:45:21 -0600

    POST "/token" with credentials returns token String. GET "/" with token String authorizes request.

--- src/main/java/com/astontech/jwtdemo/JwtDemoApplication.java
+++ src/main/java/com/astontech/jwtdemo/JwtDemoApplication.java
@@ -1,9 +1,12 @@
 package com.astontech.jwtdemo;
 
+import com.astontech.jwtdemo.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwtDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/astontech/jwtdemo/contoller/AuthController.java
@@ -0,0 +1,28 @@
+package com.astontech.jwtdemo.contoller;
+
+import com.astontech.jwtdemo.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        log.debug("Token requested for user: {}", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        log.debug("Token granted {}", token);
+        return token;
+    }
+}
